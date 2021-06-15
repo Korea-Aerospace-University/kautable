c8b4675bdbc73fe8a72dcf386a0aa35b84a2d2da
c17an <kimchanmin1@gmail.com>
2021-06-15 17:52:12 +0900

    style: 과목 리스트에 그림자 추가

--- components/subjectTable.tsx
+++ components/subjectTable.tsx
@@ -9,7 +9,7 @@ const SubjectTable = (props: Props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="flex w-full text-sm h-auto lg:max-h-[450px] scrollbar-hide overflow-scroll justify-around my-5">
+    <div className="flex w-full text-sm h-auto lg:max-h-[450px] scrollbar-hide overflow-scroll justify-around my-5 shadow-md">
       <table className="border-2 border-gray-100 w-full rounded-2xl">
         <thead className="flex flex-col lg:flex-row justify-center">
           <tr className="flex flex-1 p-2 justify-between border-b-2 border-blue-700">
@@ -22,6 +22,24 @@ const SubjectTable = (props: Props) => {
           </tr>
         </thead>
         <tbody>
+          <tr
+            className="flex lg:flex-row justify-between px-3 py-3 hover:bg-blue-100 transition-all cursor-pointer"
+            onClick={() => setIsModalOpen(true)}
+          >
+            <td className="hidden lg:block lg:w-[20%] text-center">항공우주 및 기계공학</td>
+            <td className="lg:w-[15%] text-xs lg:text-md text-center">배재성</td>
+            <td className="lg:w-[25%] text-xs lg:text-md text-center">기초기구설계</td>
+            <td className="lg:w-[10%] text-xs lg:text-md text-center">3</td>
+          </tr>
+          <tr
+            className="flex lg:flex-row justify-between px-3 py-3 hover:bg-blue-100 transition-all cursor-pointer"
+            onClick={() => setIsModalOpen(true)}
+          >
+            <td className="hidden lg:block lg:w-[20%] text-center">항공우주 및 기계공학</td>
+            <td className="lg:w-[15%] text-xs lg:text-md text-center">배재성</td>
+            <td className="lg:w-[25%] text-xs lg:text-md text-center">기초기구설계</td>
+            <td className="lg:w-[10%] text-xs lg:text-md text-center">3</td>
+          </tr>
           <tr
             className="flex lg:flex-row justify-between px-3 py-3 hover:bg-blue-100 transition-all cursor-pointer"
             onClick={() => setIsModalOpen(true)}
--- types/semester.ts
+++ types/semester.ts
@@ -4,8 +4,12 @@ export type semeterType = {
 };
 
 export const semeter = [
+  { value: "2019-1", label: "2019년 1학기" },
+  { value: "2019-2", label: "2019년 2학기" },
   { value: "2020-1", label: "2020년 1학기" },
   { value: "2020-2", label: "2020년 2학기" },
   { value: "2021-1", label: "2021년 1학기" },
   { value: "2021-2", label: "2021년 2학기" },
+  { value: "2022-1", label: "2022년 1학기" },
+  { value: "2022-2", label: "2022년 2학기" },
 ];
