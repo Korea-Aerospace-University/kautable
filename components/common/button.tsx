31e0c02dc55a15fee48f017f95e7456db7ed9ac0
c17an <kimchanmin1@gmail.com>
2021-06-18 13:27:49 +0900

    style: 버튼 테마를 항공대 컬러 테마에 맞게 수정

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<IProps> = ({ Icon, text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="flex justify-center items-center min-w-[100px] outline-none px-3 py-2 rounded-xl bg-blue-700 text-white hover:bg-blue-500 transition-all"
+      className="flex justify-center items-center min-w-[100px] outline-none px-3 py-2 rounded-xl bg-[#40368a] text-white hover:bg-blue-700 transition-all"
     >
       {Icon && <Icon className="h-5 w-5 mr-1" />}
       <div className="text-xs lg:text-md">{text}</div>
