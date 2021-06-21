a2f86e50e685b4511c9cdbe51804e2f9628c6f89
c17an <kimchanmin1@gmail.com>
2021-06-21 18:35:01 +0900

    feat: react-toast 설정 및 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import "../styles/globals.css";
 import "../styles/detail.css";
 import "tailwindcss/tailwind.css";
-import Header from "../components/header";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <div className="bg-blue-50">
       {/* <Header /> */}
       <Component />
+      <ToastContainer />
     </div>
   );
 };
