0a19ce30e03b44b9bd77f0efefe0cb9f52b0eef4
c17an <kimchanmin1@gmail.com>
2021-06-29 21:09:27 +0900

    과목 데이터 추가

---
+++ constant/semester.ts
@@ -0,0 +1,9 @@
+export const semester = [
+  { value: "2020-02", label: "2020년 2학기" },
+  { value: "2021-01", label: "2021년 1학기" },
+  { value: "2021-02", label: "2021년 2학기" },
+  { value: "2022-01", label: "2022년 1학기" },
+  { value: "2022-02", label: "2022년 2학기" },
+];
+
+export const defaultSemester = { value: "2021-02", label: "2021년 2학기" };
